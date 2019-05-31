c681be4cc2c0ce41401e2c757c0842d74108ef20
Ralph Schaer <ralphschaer@gmail.com>
2019-05-31 10:33:41 +0200

    Upgrade to Angular 8

--- chat/client/src/app/pages/messages/messages.page.ts
+++ chat/client/src/app/pages/messages/messages.page.ts
@@ -6,21 +6,21 @@ import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-messages',
-  templateUrl: 'messages.page.html',
-  styleUrls: ['messages.page.scss'],
+  templateUrl: './messages.page.html',
+  styleUrls: ['./messages.page.scss'],
 })
 export class MessagesPage implements OnInit, OnDestroy {
 
-  @ViewChild(IonContent) content: IonContent;
+  @ViewChild(IonContent, { static: true }) content: IonContent;
 
   message: string;
   messages: Message[] = [];
   roomName: string;
   showEmojiPicker = false;
 
-  @ViewChild('messageInput') messageInput: ElementRef;
+  @ViewChild('messageInput', { static: true }) messageInput: ElementRef;
 
-  @ViewChild(IonList, {read: ElementRef})
+  @ViewChild(IonList, { read: ElementRef, static: true })
   private chatElement: ElementRef;
   private mutationObserver: MutationObserver;
 
