6c93abb15e0b6ac68d4566bc2a46da01c8186d3c
Ralph Schaer <ralphschaer@gmail.com>
2021-12-16 09:54:55 +0100

    Update

--- chat/client/src/app/app.module.ts
+++ chat/client/src/app/app.module.ts
@@ -14,14 +14,13 @@ import {RoomPage} from './pages/room/room.page';
 import {SigninPage} from './pages/signin/signin.page';
 
 @NgModule({
-  declarations: [AppComponent, RelativeTimePipe, EmojiPickerComponent, AddRoomPage, MessagesPage, RoomPage, SigninPage],
-  entryComponents: [],
-  imports: [BrowserModule, CommonModule,
-    FormsModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, RelativeTimePipe, EmojiPickerComponent, AddRoomPage, MessagesPage, RoomPage, SigninPage],
+    imports: [BrowserModule, CommonModule,
+        FormsModule, IonicModule.forRoot(), AppRoutingModule],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
